477d6be7048c5beee12211d1c31a29af0674cf52
Renato Mendes <mendeslrenato@gmail.com>
2022-04-06 18:49:18 -0300

    3rd exercise/4.1

---
+++ 01_Fundaments/block04JavaScript/day01JavaScript_FirstSteps/exercise3.js
@@ -0,0 +1,9 @@
+//Compare the numbers and show the biggest one
+const num1 = 16;
+const num2 = 30;
+const num3 = 15;
+
+const firstComparison = num1 > num2 ? num1 : num2;
+
+console.log(firstComparison > num3 ? firstComparison : num3);
+
