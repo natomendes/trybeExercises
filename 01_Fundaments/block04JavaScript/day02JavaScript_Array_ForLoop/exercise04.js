e5ac7cb971c792f66c511fdf0719d95d8f7ddaa4
Renato Mendes <mendeslrenato@gmail.com>
2022-04-07 18:11:52 -0300

    4th exercise/4.2

---
+++ 01_Fundaments/block04JavaScript/day02JavaScript_Array_ForLoop/exercise04.js
@@ -0,0 +1,18 @@
+//Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let sum = 0;
+
+for (number of numbers) {
+  sum += number
+}
+
+const average = sum / numbers.length;
+
+if (average > 20) {
+  console.log('Value bigger than 20.');
+} else {
+  console.log('Value smaller than 20.');
+}
+
