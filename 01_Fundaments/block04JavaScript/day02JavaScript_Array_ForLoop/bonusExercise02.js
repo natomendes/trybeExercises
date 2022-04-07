8b4e00fcd93aa8fdd7fc35fb4f83f96ed2dea97b
Renato Mendes <mendeslrenato@gmail.com>
2022-04-07 18:56:48 -0300

    2nd bonus exercise/4.2

--- 01_Fundaments/block04JavaScript/day02JavaScript_Array_ForLoop/bonusExercise02.js
+++ 01_Fundaments/block04JavaScript/day02JavaScript_Array_ForLoop/bonusExercise02.js
@@ -1,10 +1,10 @@
-// Ordene o array numbers em ordem crescente e imprima seus valores;
+//Ordene o array numbers em ordem decrescente e imprima seus valores;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 1; i < numbers.length; i += 1){
  for (let priorIndex = 0; priorIndex < i; priorIndex += 1){
-  if (numbers[i] < numbers[priorIndex]) {
+  if (numbers[i] > numbers[priorIndex]) {
     let numberToSwap = numbers[i];
     numbers[i] = numbers[priorIndex]
     numbers[priorIndex] = numberToSwap;
