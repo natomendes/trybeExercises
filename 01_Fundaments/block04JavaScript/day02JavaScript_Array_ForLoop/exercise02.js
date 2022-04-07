c950b098553d23e7a9f284ec2325943d5aa80662
Renato Mendes <mendeslrenato@gmail.com>
2022-04-07 18:09:30 -0300

    3rd exercise/4.2

--- 01_Fundaments/block04JavaScript/day02JavaScript_Array_ForLoop/exercise02.js
+++ 01_Fundaments/block04JavaScript/day02JavaScript_Array_ForLoop/exercise02.js
@@ -2,10 +2,10 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let soma = 0;
+let sum = 0;
 
 for (number of numbers) {
-  soma += number
+  sum += number
 }
 
-console.log(soma);
+console.log(sum);
