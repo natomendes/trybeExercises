cea7d34b3771254276615d8ae981f01df7964b7e
Renato Mendes <mendeslrenato@gmail.com>
2022-04-08 20:47:30 -0300

    corrections to 1st bonus exercise/4.3

--- 01_Fundaments/block04JavaScript/day03JavaScript_ProgramLogic/bonusExercise01.js
+++ 01_Fundaments/block04JavaScript/day03JavaScript_ProgramLogic/bonusExercise01.js
@@ -1,7 +1,8 @@
 //Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n
 const drawSquare = (num) => {
+  if (num < 2) return console.log('Number needs to be bigger than 1');
   let line = '';
-  
+
   for (let i = 0; i < num; i += 1){
     line += '*';
   }
