b9c93e59940abc8603d4fec8d6fba077938653dc
Renato Mendes <mendeslrenato@gmail.com>
2022-04-08 21:39:59 -0300

    4th bonus exercise/4.3

---
+++ 01_Fundaments/block04JavaScript/day03JavaScript_ProgramLogic/bonusExercise04.js
@@ -0,0 +1,20 @@
+//Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima uma pirâmide com n asteriscos de base
+const drawSquare = (num) => {
+  if (num < 3) return console.log('Number needs to be bigger than 2');
+  if (num % 2 === 0) return console.log('Number needs to be an odd number');
+  let line = '';
+  let spacing = '';
+  for (let z = 0; z < num - 1; z += 1) {
+    spacing += ' ';
+  }
+  console.log(spacing);
+  line += '*';
+  for (let i = 0; i < num; i += 2) {
+    let cropSize = (num - i - 1) / 2;
+    console.log(spacing.slice(0, cropSize) + line + spacing.slice(0, cropSize));
+    spacing = spacing.slice(2);
+    line += '**';
+  }
+}
+
+drawSquare(11);
