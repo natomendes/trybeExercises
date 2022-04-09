c4f988b7efd621bd60f878847af14c801f0e83dc
Renato Mendes <mendeslrenato@gmail.com>
2022-04-08 21:18:32 -0300

    3rd bonus exercise/4.3

---
+++ 01_Fundaments/block04JavaScript/day03JavaScript_ProgramLogic/bonusExercise03.js
@@ -0,0 +1,17 @@
+//Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima um triângulo retângulo com 5 asteriscos de base começando da direita para a esquerda
+const drawSquare = (num) => {
+  if (num < 2) return console.log('Number needs to be bigger than 1');
+  let line = '';
+  let spacing = '';
+  for (let z = 0; z < num; z += 1) {
+    spacing += ' ';
+  }
+  console.log(spacing);
+  for (let i = 0; i < num; i += 1) {
+    line += '*';
+    spacing = spacing.slice(1);
+    console.log(spacing+line);
+  }
+}
+
+drawSquare(5);
