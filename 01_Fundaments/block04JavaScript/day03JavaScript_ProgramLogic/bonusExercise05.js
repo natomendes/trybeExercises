3408b7e4526a12b77c5e3c23c3bf927e1efa0b20
Renato Mendes <mendeslrenato@gmail.com>
2022-04-08 23:21:53 -0300

    5th bonus exercise/4.3

--- 01_Fundaments/block04JavaScript/day03JavaScript_ProgramLogic/bonusExercise05.js
+++ 01_Fundaments/block04JavaScript/day03JavaScript_ProgramLogic/bonusExercise05.js
@@ -8,12 +8,23 @@ const drawSquare = (num) => {
     spacing += ' ';
   }
   console.log(spacing);
-  line += '*';
+  line = '*';
+  let middleSpace = spacing;
   for (let i = 0; i < num; i += 2) {
     let cropSize = (num - i - 1) / 2;
+
+    if (i + 1 === num) {
+      let lastLine = '';
+      for (let y = 0; y < num; y += 1) {
+        lastLine += '*';
+      }
+      console.log(lastLine);
+      break;
+    }
+
     console.log(spacing.slice(0, cropSize) + line + spacing.slice(0, cropSize));
+    line = '*' + middleSpace.slice(0, i + 1) + '*';
     spacing = spacing.slice(2);
-    line += '**';
   }
 }
 
