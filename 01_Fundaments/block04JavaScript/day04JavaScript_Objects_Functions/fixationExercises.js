c10c1c8a53b141a8b169ce36094dc0a3f95eb44c
Renato Mendes <mendeslrenato@gmail.com>
2022-04-11 15:40:03 -0300

    2nd fixation exercise/4.4

--- 01_Fundaments/block04JavaScript/day04JavaScript_Objects_Functions/fixationExercises.js
+++ 01_Fundaments/block04JavaScript/day04JavaScript_Objects_Functions/fixationExercises.js
@@ -13,3 +13,22 @@ console.log(`A jogadora ${player.name} ${player.lastName} tem ${player['age']} a
 console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player['bestInTheWorld'].length} vezes.`);
 console.log(`A jogadora ${player.name} ${player.lastName} possui ${player.medals.golden} medalhas de ouro e ${player['medals'].silver} medalhas de prata.`);
 
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let key in names) {
+  console.log(`Olá ${names[key]}!`);
+}
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let index in car) {
+  console.log(`${index}: ${car[index]}`)
+}
