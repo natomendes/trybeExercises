5a929b996f54c35ec0dd8e8b80941a1dada7b984
Renato Mendes <mendeslrenato@gmail.com>
2022-04-14 15:14:57 -0300

    1st fixation exercise/5.2

---
+++ 01_Fundaments/block05Dom_Events_WebStorage/day02WorkingWithElements/fixationExercises/script.js
@@ -0,0 +1,19 @@
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.backgroundColor = 'green';
+
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.padding = '10px';
+
+document.querySelector('#primeiroFilhoDoFilho').innerText = 'Adicionado texto ao elemento primeiroFilhoDoFilho.';
+
+console.log(document.querySelector('#pai').firstElementChild);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
+
+console.log(document.querySelector('#pai').children[2]);
+
+
+
+
