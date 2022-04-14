042762cc6adb5ac6a89adace171522a489faf008
Renato Mendes <mendeslrenato@gmail.com>
2022-04-14 15:41:00 -0300

    2nd fixation exercise/5.2

--- 01_Fundaments/block05Dom_Events_WebStorage/day02WorkingWithElements/fixationExercises/script.js
+++ 01_Fundaments/block05Dom_Events_WebStorage/day02WorkingWithElements/fixationExercises/script.js
@@ -14,6 +14,22 @@ console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
 
 console.log(document.querySelector('#pai').children[2]);
 
+const filho = document.createElement('section');
+
+document.querySelector('#pai').appendChild(filho);
+
+const filho2 = document.createElement('section');
+
+document.querySelector('#elementoOndeVoceEsta').appendChild(filho2);
+
+const filho3 = document.createElement('section');
+
+filho3.id = 'filho3';
+
+document.querySelector('#elementoOndeVoceEsta').firstElementChild.appendChild(filho3);
+
+console.log(document.querySelector('#filho3').parentElement.parentElement.nextElementSibling);
+
 
 
 
