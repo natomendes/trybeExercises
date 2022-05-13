b0dfc2fa70c6b956085f604a1ba2b2f6cd654888
Renato Mendes <mendeslrenato@gmail.com>
2022-05-13 15:54:52 -0300

    exercise3/8.1

---
+++ 01_Fundaments/block08Hofs_ES6/day01Intro_Hofs/exercise3.js
@@ -0,0 +1,16 @@
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const mapStudentScore = (rightAnswers, studentAnswers) => (
+  studentAnswers.map((value, index) => {
+    if (value === 'N.A') return 0;
+    if (value === rightAnswers[index]) return 1;
+    return 0.5;
+  })
+)
+
+const getTestScore = (rightAnswers, studentAnswers, func) => (
+  func(rightAnswers, studentAnswers).reduce((acc, value) => acc + value)
+);
+
+console.log(getTestScore(RIGHT_ANSWERS, STUDENT_ANSWERS, mapStudentScore));
