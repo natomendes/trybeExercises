16412d6b19ff83b83074137e9ca296df13e31fb3
Renato Mendes <mendeslrenato@gmail.com>
2022-05-16 17:31:13 -0300

    fixation exercise/8.2

---
+++ 01_Fundaments/block08Hofs_ES6/day02.find.sort.every.some.forEach/fixationExercises.js
@@ -0,0 +1,36 @@
+// 1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false, use some;
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => arr.some((element) => element === name);
+
+console.log(hasName(names, 'Anas'))
+
+// 2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false, use every;
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => arr.every((element) => element.age >= minimumAge);  
+
+console.log(verifyAges(people, 18));
+
+// 3 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente.
+// ordem crescente
+console.log(people.sort((a, b) => a.age - b.age));
+// ordem decrescente
+console.log(people.sort((a, b) => b.age - a.age));
+
+console.log(people.sort((a, b) => {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+}));
