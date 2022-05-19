2e4aed84527eae4217642f240a94007aa6e3711b
Renato Mendes <mendeslrenato@gmail.com>
2022-05-19 17:09:57 -0300

    exercise4/8.5

--- 01_Fundaments/block08Hofs_ES6/day05Spread_rest_descontructing../exercises.js
+++ 01_Fundaments/block08Hofs_ES6/day05Spread_rest_descontructing../exercises.js
@@ -36,5 +36,43 @@ const gunnar = {
 // complete a assinatura da função abaixo
 const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
-console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
-console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+// console.log(personLikes(alex)); 
+// console.log(personLikes(gunnar)); 
+
+// 🚀 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = () => people.filter(({bornIn, nationality}) => (
+  nationality === 'Australian' && bornIn > 1900 && bornIn <= 2000
+))
+
+console.log(filterPeople());
+
