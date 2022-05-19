79c27780093de10cbb9c415997d54e4d0ae15af4
Renato Mendes <mendeslrenato@gmail.com>
2022-05-19 17:04:10 -0300

    exercise2-3/8.5

--- 01_Fundaments/block08Hofs_ES6/day05Spread_rest_descontructing../exercises.js
+++ 01_Fundaments/block08Hofs_ES6/day05Spread_rest_descontructing../exercises.js
@@ -8,6 +8,33 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  console.log(rectangleArea(...rectangle))
+  // console.log(rectangleArea(...rectangle))
 });
 
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+
+const sum = (...params) => params.reduce((acc, value) => acc + value);
+
+// console.log(sum(1, 2, 3, 4, 5, 6));
+
+// 3 - Escreva a função personLikes, que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
