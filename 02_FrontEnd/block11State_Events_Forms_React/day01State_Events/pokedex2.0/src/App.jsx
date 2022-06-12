8150a3730483ba4e8c74b1f4e47354e5a9da9fe2
Renato Mendes <mendeslrenato@gmail.com>
2022-06-11 22:44:39 -0300

    exercise/11.1

---
+++ 02_FrontEnd/block11State_Events_Forms_React/day01State_Events/pokedex2.0/src/App.jsx
@@ -0,0 +1,12 @@
+import './css/App.css'
+import Pokedex from './components/Pokedex'
+
+function App() {
+  return (
+    <div className='App'>
+      <Pokedex />
+    </div>
+  )
+}
+
+export default App
